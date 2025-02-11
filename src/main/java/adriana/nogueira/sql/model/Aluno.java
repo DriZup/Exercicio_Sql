63db65f2a4399828e439bed8c4fb38ba54bea470
Adriana Borges Nogueira <adriana.nogueira@zup.com.br>
2025-02-11 13:45:00 -0300

    feature: criando classe model

---
+++ src/main/java/adriana/nogueira/sql/model/Aluno.java
@@ -0,0 +1,44 @@
+package adriana.nogueira.sql.model;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private int idade;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
